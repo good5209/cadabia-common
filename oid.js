0c15ca531489ad14b4409271b028dcee26530c08
good5209 <good5209@gmail.com>
2014-04-29 17:34:14 +0800

    Modify Oid indicate only one object

--- oid.js
+++ oid.js
@@ -3,22 +3,21 @@
  * Object ID
  * prefix: prefix, clazz: class name, object: object list
  */
-function Oid(prefix, clazz, objects) {
+function Oid(prefix, clazz, object) {
 	// prefix
-	if (_.isString(prefix)) {
-		this.prefix = prefix;
-	} else {
-		this.prefix = null;
-	}
+	this.prefix = (_.isString(prefix)
+		? prefix
+		: null);
 	
 	// class
-	if (_.isString(clazz)) {
-		this.class = clazz;
-	}
+	this.class = (_.isString(clazz)
+		? clazz
+		: null);
 	
 	// objects
-	this.objects = [];
-	this.addObjects(objects);
+	this.object = (_.isString(object)
+		? object
+		: null);
 }
 
 // static method
@@ -37,20 +36,6 @@ Oid.parseJson = function (json) {
 
 // instance method
 /*
- * add objects into this Oid
- */
-Oid.prototype.addObjects = function(objects) {
-	var self = this;
-	if (!_.isArray(objects)) { // convert argument to array
-		var objects = [objects];
-	}
-	self.objects = _.union(
-		self.objects,
-		_.filter(objects, _.isString)); // keep string elements only
-	self.objects.sort();
-}
-
-/*
  * get prefix string
  */
 Oid.prototype.getPrefix = function () {
@@ -67,12 +52,8 @@ Oid.prototype.getClass = function () {
 /*
  * get objects string array
  */
-Oid.prototype.getObjects = function () {
-	var self = this;
-	return (
-		_.isEmpty(self.objects)
-		? null
-		: self.objects);
+Oid.prototype.getObject = function () {
+	return this.object;
 }
 
 /*
@@ -87,18 +68,12 @@ Oid.prototype.toString = function () {
 	
 	var classString = '@' + self.getClass();
 	
-	var objectsString = self.getObjects();
-	objectsString = (objectsString === null
+	var objectString = self.getObject();
+	objectString = (objectString === null
 		? ''
-		: '['
-			// concatenate all objects as string, between with ';'
-			+ _.reduce(
-				_.rest(objectsString), // remain elements
-				function (memo, each) {return memo + ';' + each;}, // concat function
-				_.first(objectsString)) // first element
-			+ ']');
+		: '[' + objectString + ']');
 	
-	return prefixString + classString + objectsString;
+	return prefixString + classString + objectString;
 }
 
 // export
--- oid_tests.js
+++ oid_tests.js
@@ -12,12 +12,12 @@ Tinytest.add('Oid - check field', function (test) {
 	var oid = new Cadabia.Oid('prefix', 'class', 'object');
 	test.equal(oid.prefix, 'prefix');
 	test.equal(oid.class, 'class');
-	test.equal(oid.objects, ['object']);
+	test.equal(oid.object, 'object');
 	
 	var oid = new Cadabia.Oid('prefix2', 'class2', 'object2');
 	test.equal(oid.prefix, 'prefix2');
 	test.equal(oid.class, 'class2');
-	test.equal(oid.objects, ['object2']);
+	test.equal(oid.object, 'object2');
 });
 
 Tinytest.add('Oid - getPrefix', function (test) {
@@ -40,43 +40,23 @@ Tinytest.add('Oid - getClass', function (test) {
 	test.equal(oid.getClass(), 'class2');
 });
 
-Tinytest.add('Oid - getObjects', function (test) {
-	var oid = new Cadabia.Oid('prefix', 'class', 'object');
-	test.equal(oid.getObjects(), ['object']);
+Tinytest.add('Oid - getObject', function (test) {
+	var oid = new Cadabia.Oid('prefix', 'class', 'object1');
+	test.equal(oid.getObject(), 'object1');
 	
-	oid = new Cadabia.Oid('prefix', 'class', ['object']);
-	test.equal(oid.getObjects(), ['object']);
-	
-	oid = new Cadabia.Oid('prefix', 'class', ['object1', 'object2']);
-	test.equal(oid.getObjects(), ['object1', 'object2']);
-	
-	// object will sorted
-	oid = new Cadabia.Oid('prefix', 'class', ['b', 'a', 'c']);
-	test.equal(oid.getObjects(), ['a', 'b', 'c']);
+	oid = new Cadabia.Oid('prefix', 'class', 'object2');
+	test.equal(oid.getObject(), 'object2');
 	
 	// null object, meaning all objects
 	oid = new Cadabia.Oid('prefix', 'class', null);
-	test.equal(oid.getObjects(), null);
+	test.equal(oid.getObject(), null);
 	
+	// not string object will be convert to null
 	oid = new Cadabia.Oid('prefix', 'class', true);
-	test.equal(oid.getObjects(), null);
+	test.equal(oid.getObject(), null);
 	
-	oid = new Cadabia.Oid('prefix', 'class', [0, 1, false]);
-	test.equal(oid.getObjects(), null);
-});
-
-Tinytest.add('Oid - addObjects', function (test) {
-	var oid = new Cadabia.Oid('prefix', 'class', 'a');
-	test.equal(oid.getObjects(), ['a']);
-	
-	oid.addObjects('e');
-	test.equal(oid.getObjects(), ['a', 'e']);
-	
-	oid.addObjects('c');
-	test.equal(oid.getObjects(), ['a', 'c', 'e']);
-	
-	oid.addObjects(['0', 'd', 'f', 'b', 'a']);
-	test.equal(oid.getObjects(), ['0', 'a', 'b', 'c', 'd', 'e', 'f']);
+	oid = new Cadabia.Oid('prefix', 'class', ['object', 1, false]);
+	test.equal(oid.getObject(), null);
 });
 
 Tinytest.add('Oid - toString', function (test) {
@@ -89,12 +69,6 @@ Tinytest.add('Oid - toString', function (test) {
 	oid = new Cadabia.Oid(null, 'class', 'object');
 	test.equal(oid.toString(), '@class[object]');
 	
-	oid = new Cadabia.Oid(null, 'class', ['object1', 'object2']);
-	test.equal(oid.toString(), '@class[object1;object2]');
-	
 	oid = new Cadabia.Oid('prefix', 'class', 'object');
 	test.equal(oid.toString(), 'prefix:@class[object]');
-	
-	oid = new Cadabia.Oid('prefix', 'class', ['object1', 'object2']);
-	test.equal(oid.toString(), 'prefix:@class[object1;object2]');
 });
